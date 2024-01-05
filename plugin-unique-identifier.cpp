137bc24d7d3e979e7ca3c19bfe29e6e377f86a18
karl yerkes <karl.yerkes@gmail.com>
2024-01-04 17:19:18 -0800

    Update plugin-unique-identifier.cpp

--- plugin-unique-identifier.cpp
+++ plugin-unique-identifier.cpp
@@ -10,15 +10,6 @@ int main(int argc, char* argv[]) {
   manager.addDefaultFormats();
   juce::String file(argv[1]);
   juce::OwnedArray<juce::PluginDescription> description;
-//  for (int i = 0; i < manager.getNumFormats(); i++) {
-//    auto* format = manager.getFormat(i);
-//    format->findAllTypesForFile(description, file);
-//    printf("%d\n", description.size());
-//    for (int k = 0; k < description.size(); k++) {
-//      printf("%x\n", description[0]->uniqueId);
-//      return 0;
-//    }
-//  }
   auto* format = manager.getFormat(0);
   format->findAllTypesForFile(description, file);
   printf("%x\n", description[0]->uniqueId);
